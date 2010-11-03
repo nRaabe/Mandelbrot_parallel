636d9125290b955e6fa0e2aea9c9424d6bd74b2d
nRaabe <nils182@freenet.de>
2010-11-03 22:06:41 +0100

    Fibonacci Programm hinzugefuegt

---
+++ Main.hs
@@ -0,0 +1,45 @@
+module Main where
+    
+import System.Time
+import Control.Parallel
+
+    fib :: Int -> Int 
+    fib 0 = 0 
+    fib 1 = 1 
+    fib n = fib (n-1) + fib (n-2)
+    
+    euler :: Int -> Int
+    euler n = length (filter (relprime n) (mkList n)) 
+    
+    sumEuler :: Int -> Int
+    sumEuler = sum . (map euler) . mkList
+    
+    mkList :: Int -> [Int]
+    mkList n = [1..n-1] 
+    
+    relprime :: Int -> Int -> Bool
+    relprime x y = gcd x y == 1 
+        
+    sumFibEuler :: Int -> Int -> Int
+    sumFibEuler a b = fib a + sumEuler b
+    
+    parSumFibEuler :: Int -> Int -> Int 
+    parSumFibEuler a b = par f ( e + f )
+                        where
+                            f = fib a 
+                            e = sumEuler b
+            
+    secDiff :: ClockTime -> ClockTime -> Float 
+    secDiff (TOD secs1 psecs1) (TOD secs2 psecs2) = fromInteger (psecs2 - psecs1) / 1e12 + fromInteger (secs2 - secs1)
+    
+    r1 :: Int 
+    r1 = parSumFibEuler 38 5300
+    
+    main :: IO () 
+    main = do 
+            t0 <- getClockTime 
+            pseq r1 (return ())
+            t1 <- getClockTime 
+            
+            putStrLn ("sum: " ++ show r1) 
+            putStrLn ("time: " ++ show (secDiff t0 t1) ++ " seconds")
